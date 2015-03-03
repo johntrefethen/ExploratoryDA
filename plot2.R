ab3e9d4790a42af416433f489829b4582a33cd55
John Trefethen <trefethens@gmail.com>
2015-03-03 09:03:59 -0700

    Initial commit
    Plot2 initial commit.

---
+++ plot2.R
@@ -0,0 +1,31 @@
+# plot2.R
+# Reads power consumption data and creates a histogram
+# Load package 'dplyr' so we can use filter() function
+require(dplyr)
+
+# Read the primary data file
+# power <- read.csv("household_power_consumption.txt", sep = ";")
+
+# For testing, read only filtered file
+power <- read.csv("power_filter.csv")
+
+# Change Date column to an r date class
+power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
+
+# Filter for only the dates we want
+pf <- filter(power, Date == "2007-02-01" | Date == "2007-02-02")
+
+# Build the histogram
+pf_hist <- hist(power$Global_active_power, nclass = 12, plot = FALSE)
+
+# Create the png file
+png("plot1.png")
+plot(pf_hist, col = "Red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency")
+dev.off()
+
+# Print output to console
+plot(pf_hist, col = "Red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency")
