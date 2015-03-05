229efc83055e78534bd4766a285fdd82b4448207
John Trefethen <trefethens@gmail.com>
2015-03-04 21:13:15 -0700

    Plot 2 - code and graph
    Initial commit for the code and graph for plot 2 of course.

--- plot2.R
+++ plot2.R
@@ -4,10 +4,11 @@
 require(dplyr)
 
 # Read the primary data file
-# power <- read.csv("household_power_consumption.txt", sep = ";")
-
-# For testing, read only filtered file
-power <- read.csv("power_filter.csv")
+# Assumes user has unzipped the source file to their
+# working directory.
+power <- read.table("household_power_consumption.txt", 
+                     header = TRUE, sep = ";", na.strings = "?", 
+                     colClasses = c("Global_active_power" = "numeric"))
 
 # Change Date column to an r date class
 power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
@@ -15,17 +16,19 @@ power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
 # Filter for only the dates we want
 pf <- filter(power, Date == "2007-02-01" | Date == "2007-02-02")
 
-# Build the histogram
-pf_hist <- hist(power$Global_active_power, nclass = 12, plot = FALSE)
+# Combine Date and Time into a single value
+timedt <- as.POSIXct(paste(pf$Date, pf$Time))
 
-# Create the png file
-png("plot1.png")
-plot(pf_hist, col = "Red", main = "Global Active Power", 
-     xlab = "Global Active Power (kilowatts)", 
-     ylab = "Frequency")
+# Create the png file - open device
+png("plot2.png")
+
+# Create the plot
+plot(timedt, pf$Global_active_power, type = "l", 
+     ylab = "Global Active Power (kilowatts)", xlab = "")
+
+# Close the device
 dev.off()
 
 # Print output to console
-plot(pf_hist, col = "Red", main = "Global Active Power", 
-     xlab = "Global Active Power (kilowatts)", 
-     ylab = "Frequency")
+plot(timedt, pf$Global_active_power, type = "l", 
+     ylab = "Global Active Power (kilowatts)", xlab = "")
